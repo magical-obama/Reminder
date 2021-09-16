6497ad6e73be56f4f8f2806dc76c474283a03a21
magical-obama <72757458+magical-obama@users.noreply.github.com>
2021-09-16 22:04:51 +0200

    Add Floating Button TODO

--- app/src/main/java/com/max/reminder/MainActivity.kt
+++ app/src/main/java/com/max/reminder/MainActivity.kt
@@ -2,10 +2,19 @@ package com.max.reminder
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<FloatingActionButton>(R.id.add_item_button).setOnClickListener {
+            //TODO Implement Add Item Activity
+        }
     }
+
+
+
 }
