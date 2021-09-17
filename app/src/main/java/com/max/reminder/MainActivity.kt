98a6357dbad3ff3f234c6e4267cb06c4ba1bc217
magical-obama <72757458+magical-obama@users.noreply.github.com>
2021-09-17 18:49:33 +0200

    Version 0.0.2

--- app/src/main/java/com/max/reminder/MainActivity.kt
+++ app/src/main/java/com/max/reminder/MainActivity.kt
@@ -3,6 +3,8 @@ package com.max.reminder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
@@ -10,9 +12,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        findViewById<FloatingActionButton>(R.id.add_item_button).setOnClickListener {
-            //TODO Implement Add Item Activity
+        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener {
+            Toast.makeText(this, "Not yet implemented", Toast.LENGTH_SHORT).show()
         }
+
+        val items = arrayOf("Item 1", "Item 2", "Item 3", "Item 4", "Item 5", "Item 6", "Item 7", "Item 8", "Item 9", "Item 10")
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+        recyclerView.adapter = ReminderAdapter(items)
+        recyclerView.layoutManager = LinearLayoutManager(this)
     }
 
 
---
+++ app/src/main/java/com/max/reminder/ReminderAdapter.kt
@@ -0,0 +1,30 @@
+package com.max.reminder
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class ReminderAdapter(private val dataSet: Array<String>) : RecyclerView.Adapter<ReminderAdapter.ViewHolder>() {
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val textView: TextView
+
+        init {
+            textView = view.findViewById(R.id.textView)
+        }
+    }
+
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(viewGroup.context).inflate(R.layout.reminder_item, viewGroup, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
+        viewHolder.textView.text = dataSet[position]
+    }
+
+    override fun getItemCount() = dataSet.size
+
+}
